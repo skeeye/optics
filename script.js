be8c2b2042ea7fb59dc1793268d5741a6ac39508
skeeye <andreycater77@gmail.com>
2025-05-28 04:20:55 +0300

    aa

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-
-function toggleTheme() {
-  const body = document.body;
-  body.classList.toggle("dark-mode");
-
-  const btn = document.getElementById("theme-btn");
-  btn.textContent = body.classList.contains("dark-mode") 
-    ? "Увімкнути світлу тему" 
-    : "Увімкнути темну тему";
-}
+document.getElementById('toggle-theme').addEventListener('click', () => {
+  document.body.classList.toggle('light-mode');
+});
